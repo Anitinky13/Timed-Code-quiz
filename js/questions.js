9165ccd6a6e45fe9f377ee364202d120e9977b10
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-05-17 14:49:25 -0500

    add event listener

--- js/questions.js
+++ js/questions.js
@@ -1,1 +1,11 @@
+var buttonEl = document.querySelector("#start");
+console.log(buttonEl);
+var buttonEl = document.querySelector("#start");
 
+buttonEl.addEventListener("click", function () {
+  var listItem = document.createElement("li");
+  listItem.textContent = "New Question";
+  question.appendChild(ulCreate);
+  ulCreate.appendChild(listItem);
+  listItem.addEventListener("click", compare);
+});
