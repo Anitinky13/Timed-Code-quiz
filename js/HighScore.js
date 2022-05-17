b4c1d0a4fd416119aa346af1443eca55b2635304
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-05-17 14:22:59 -0500

    css outline per html document

--- js/HighScore.js
+++ js/HighScore.js
@@ -1,14 +1,3 @@
-var questions = document.getElementById("questions");
-var time = document.getElementById("time");
-var choices = document.getElementById("choices");
-var submit = document.getElementById("submit");
-var start = document.getElementById("start");
-
-function startQuiz() {
-  var startScreen = document.getElementById("start-screen");
-  startScreen.setAttribute("class", ".hide");
-
-  time = setInterval(clockTick, 1000);
-}
-
-start.onClick = startQuiz;
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var back = document.querySelector("#back");
