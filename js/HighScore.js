3f6fb7201fd03732102a95344adf433686895b2e
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-05-16 20:04:50 -0500

    start

---
+++ js/HighScore.js
@@ -0,0 +1,16 @@
+var questions = document.getElementById("questions");
+var time = document.getElementById("time");
+var choices = document.getElementById("choices");
+var submit = document.getElementById("submit");
+var start = document.getElementById("start");
+
+function startQuiz() {
+  var startScreen = document.getElementById("start-screen");
+  startScreen.setAttribute("class", "hide");
+
+  questions.removeAttribute("class");
+
+  time = setInterval(clockTick, 1000);
+}
+
+start.onClick = startQuiz;
