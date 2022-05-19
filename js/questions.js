b1464c68df2c4f5eca6cfa8d750c0de681f2dffb
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-05-19 18:07:02 -0500

    array of objects-questions, choices and answers

--- js/questions.js
+++ js/questions.js
@@ -1,7 +1,45 @@
-var buttonEl = document.querySelector("#start");
-choice.forEvery(function (newQuestion) {
-  var listItem = document.createElement("li");
-  listItem.textContent = newQuestion;
-  questions.appendChild(listItem);
-  listItem.addEventListener("click", compare);
-});
+//Array of Objects=questions
+var myQuestions = [
+  {
+    question: "Commonly used data types Do Not include:",
+
+    choice: ["Strings", "Booleans", "Alerts", "Numbers"],
+    correctAnswer: "Alerts",
+  },
+  {
+    question: "The condition in an if/else statement is enclosed within___.",
+    choice: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+    correctAnswer: "Parenthesis",
+  },
+  {
+    question: "Arrays in Javascript can be used to store____.",
+    choice: [
+      "Number and Strings",
+      "Other Arrays",
+      "Booleans",
+      "All Of The Above",
+    ],
+    correctAnswer: "All Of The Above",
+  },
+  {
+    question:
+      "String values must be enclosed within___ when being assigned to variables.",
+    choice: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+    correctAnswer: "Quotes",
+  },
+
+  {
+    question:
+      "A very useful tool for used during development and debugging for printing content to the debugger is:",
+    choice: ["Javascript", "Terminal/Bash", "For Loops", "Console Log"],
+    correctAnswer: "Console Log",
+  },
+];
+//each question object need a question value, a choices,array choices,answer value
+//loop through the array
+//variable that holds the index number of the current question array that your on
+//hardcore the 4 choices button in the html
+//then set the text contents of the buttons in that loop
+//have a hardcore spot for the question
+//work on timer
+//work on highscores
