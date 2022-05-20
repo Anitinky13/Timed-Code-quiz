f1bd288b6c6b928b92d54c6121fe73f285e69d6e
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-05-20 11:37:00 -0500

    variable declared

--- js/questions.js
+++ js/questions.js
@@ -1,18 +1,27 @@
+var quizContainer = document.querySelector("#quizz");
+var start = document.querySelector("#startQuiz");
+var quizContainer = document.querySelector("#container");
+var currentTime = document.querySelector("#time");
+var questionsEl = document.querySelector("#questions");
+
 //Array of Objects=questions
 //each question object need a question value, a choices,array choices,answer value
 var myQuestions = [
   {
+    number: 1,
     question: "Commonly used data types Do Not include:",
 
     choice: ["Strings", "Booleans", "Alerts", "Numbers"],
     correctAnswer: "Alerts",
   },
   {
+    number: 2,
     question: "The condition in an if/else statement is enclosed within___.",
     choice: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
     correctAnswer: "Parenthesis",
   },
   {
+    number: 3,
     question: "Arrays in Javascript can be used to store____.",
     choice: [
       "Number and Strings",
@@ -23,6 +32,7 @@ var myQuestions = [
     correctAnswer: "All Of The Above",
   },
   {
+    number: 4,
     question:
       "String values must be enclosed within___ when being assigned to variables.",
     choice: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
@@ -30,17 +40,19 @@ var myQuestions = [
   },
 
   {
+    number: 5,
     question:
       "A very useful tool for used during development and debugging for printing content to the debugger is:",
     choice: ["Javascript", "Terminal/Bash", "For Loops", "Console Log"],
     correctAnswer: "Console Log",
   },
 ];
-var question = document.getElementById("question");
-var time = document.getElementById("time");
-var choices = document.getElementById("choice");
-var submit = document.getElementById("submit");
-var start = document.getElementById("start");
+//variables
+var startBtn = document.querySelector(".btn");
+
+//for each question
+
+//variable to store the list of possible answers
 
 //loop through the array
 //variable that holds the index number of the current question array that your on
